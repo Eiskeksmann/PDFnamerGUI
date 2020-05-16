5b8621a76a8044d2bca588336660a47ec87c6ade
Administrator <mrbennsko@gmail.com>
2020-05-16 16:38:44 +0200

    - SYNC - HOME OFFICE -> All GUI OBJECTS READ IN!

--- src/sample/sampleController.java
+++ src/sample/sampleController.java
@@ -1,9 +1,10 @@
 package sample;
 
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
@@ -11,8 +12,33 @@ import java.util.ResourceBundle;
 
 public class sampleController implements Initializable {
 
+    //Main View
     @FXML private Button cmd_scan, cmd_sheet, cmd_algorithm, cmd_settings;
     @FXML private Pane pan_scan, pan_sheet, pan_algorithm, pan_settings;
+
+    //Scan Pane
+    @FXML private Button cmd_scan_path;
+    @FXML private TextField txt_scan_path;
+    @FXML private Label lbl_lst_scan;
+    @FXML private ListView lst_scan;
+
+    //Sheet Pane
+    @FXML private Button cmd_sheet_path;
+    @FXML private TextField txt_sheet_path;
+    @FXML private Label lbl_sheet_scan;
+    @FXML private ListView lst_sheet;
+
+    //Algorithm Pane
+    @FXML private Button cmd_run_algorithm;
+    @FXML private FontAwesomeIconView icon_scan, icon_sheet, icon_output;
+    @FXML private ProgressBar pbar_algorithm;
+
+    //Settings Pane
+    @FXML private Button cmd_settings_output_path, cmd_settings_settings_path, cmd_settings_bill_type,
+            cmd_settings_log_path, cmd_settings_confirm;
+    @FXML private TextField txt_settings_output_path, txt_settings_log_path, txt_settings_settings_path;
+    @FXML private ChoiceBox com_settings_bill_type;
+
 
     private PaneManager pm;
 
